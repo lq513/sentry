4da66804ad3fe20f74e60fe4df5e61d07cc12a1a
Marcos Gaeta <marcos.gaeta@sentry.io>
2022-02-07 13:24:52 -0800

    Add types to relay endpoints

--- src/sentry/api/endpoints/relay/__init__.py
+++ src/sentry/api/endpoints/relay/__init__.py
@@ -22,7 +22,7 @@ __all__ = (
 )
 
 
-class RelayIdSerializer(serializers.Serializer):
+class RelayIdSerializer(serializers.Serializer):  # type: ignore
     relay_id = serializers.RegexField(
         r"^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$", required=True
     )
